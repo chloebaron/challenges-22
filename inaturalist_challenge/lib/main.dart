7c280f33766055fba6a4a1d5257f9133d59ad4dc
Chloe Baron <chloebaron@chloebaron-1904.local>
2022-07-24 11:38:42 -0300

    List of spiders observed in bda in 2022. Need to separate widgets and add buttons to control data selected

--- inaturalist_challenge/lib/main.dart
+++ inaturalist_challenge/lib/main.dart
@@ -24,16 +24,23 @@ class _MyHomePageState extends State<MyHomePage> {
     http.Response response;
     // CHANGE THIS CALL TO MORE THAN ONE ID
     // ADD A BUTTON TO SWITCH BETWEEN TYPES OF BUTTONS
-    var url =
-        Uri.parse('https://api.inaturalist.org/v1/observations/127458555');
+
+    // MARLIN URI
+    // var url =
+    //     Uri.parse('https://api.inaturalist.org/v1/observations/127458555');
+    // BERMUDA SPIDERS 2022 URI
+    var url = Uri.parse(
+        'https://api.inaturalist.org/v1/observations?photos=true&place_id=8638&year=2022&iconic_taxa=Arachnida&order=desc&order_by=created_at');
 
     response = await http.get(url);
     // print('Response status: ${response.statusCode}');
     // print('Response body: ${response.body}');
-    setState(() {
-      mapResponse = json.decode(response.body);
-      listOfResults = mapResponse['results'];
-    });
+    if (response.statusCode == 200) {
+      setState(() {
+        mapResponse = json.decode(response.body);
+        listOfResults = mapResponse['results'];
+      });
+    }
   }
 
   @override
@@ -48,20 +55,56 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: const Text('iNaturalist Code Challenge'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              "Results Total: ${mapResponse['total_results'].toString()}",
+      body: mapResponse == null
+          ? Container()
+          : Center(
+              child: SingleChildScrollView(
+                child: Column(
+                  // mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    // Image.network(listOfResults[0]['default_photo']['url'].toString()),
+                    Text(
+                      "Spiders Observed in Bermuda in 2022: ${mapResponse['total_results'].toString()}",
+                      style: const TextStyle(fontSize: 24),
+                    ),
+
+                    //CHANGE THIS TO LIST BUILDER
+                    ListView.builder(
+                      itemCount: listOfResults.length,
+                      shrinkWrap: true,
+                      itemBuilder: (context, index) {
+                        return Container(
+                          padding: const EdgeInsets.all(10),
+                          height: 100,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              // Image.network(
+                              //     listOfResults[index]['images']['url']),
+                              Text(
+                                "Species: ${listOfResults[index]['species_guess']}",
+                              ),
+                              Text(
+                                "Name: ${listOfResults[index]['taxon']['name']}",
+                              ),
+                              Text(
+                                "Species ID: ${listOfResults[index]['id']}",
+                              ),
+                              // Text(listOfResults[index]['description'])
+                            ],
+                          ),
+                        );
+                      },
+                    )
+
+                    // Text(
+                    //   "Species: ${listOfResults[0]['species_guess'].toString()}",
+                    // ),
+                    // Text(listOfResults[0]['description'])
+                  ],
+                ),
+              ),
             ),
-            //CHANGE THIS TO LIST BUILDER
-            Text(
-              "Species: ${listOfResults[0]['species_guess'].toString()}",
-            ),
-          ],
-        ),
-      ),
       // floatingActionButton: FloatingActionButton(
       //   onPressed: () async {
       //     get
