1c06cd25b230806563c436798980c05e6429f001
Chloe Baron <chloebaron@chloebaron-1904.local>
2022-08-12 13:17:40 -0300

    fixed scrolling but removed title at top. Not happy with this particular fix but will rework

--- inaturalist_challenge/lib/views/introduced_view.dart
+++ inaturalist_challenge/lib/views/introduced_view.dart
@@ -11,86 +11,76 @@ class IntroducedView extends StatefulWidget {
 }
 
 class _IntroducedViewState extends State<IntroducedView> {
-  // late Future<List> futureIntroduced;
-
-  // @override
-  // void initState() {
-  //   setState(() {
-  //     // futureIntroduced = getIntroduced();
-  //   });
-
-  //   super.initState();
-  // }
-
   @override
   Widget build(BuildContext context) {
     return introducedResponse['results'] == null
         ? Container()
         : Scaffold(
-            body: Center(
-                child: SingleChildScrollView(
-              child: Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  // mainAxisAlignment: MainAxisAlignment.start,
-                  children: <Widget>[
-                    // NavButtons(),
-                    const Text(
-                      'Introduced Plants Observed in July 2022',
-                      style:
-                          TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-                    ),
-                    Text(
-                      'Observation Count: ${introducedResponse['total_results'].toString()}',
-                      style: const TextStyle(
-                          fontSize: 18, fontWeight: FontWeight.bold),
-                    ),
-                    FutureBuilder(
-                        future: getIntroduced(),
-                        builder: (context, snapshot) {
-                          return ListView.builder(
-                            itemCount: listOfIntroduced.length,
-                            shrinkWrap: true,
-                            itemBuilder: (context, index) {
-                              return listOfIntroduced[index]['species_guess'] ==
-                                      null
-                                  ? Container()
-                                  : Container(
-                                      padding: const EdgeInsets.symmetric(
-                                          vertical: 10),
-                                      // height: 100,
-                                      child: Column(
-                                        crossAxisAlignment:
-                                            CrossAxisAlignment.start,
-                                        children: <Widget>[
-                                          // Image.network(listOfIntroduced[index]['images']['url']),
-                                          Text(
-                                              "Common Name: ${listOfIntroduced[index]['species_guess']}",
-                                              style: const TextStyle(
-                                                  fontSize: 16,
-                                                  fontWeight: FontWeight.bold)),
-                                          Text(
-                                              "Species Name: ${listOfIntroduced[index]['taxon']['name']}",
-                                              style: const TextStyle(
-                                                  fontSize: 16,
-                                                  fontStyle: FontStyle.italic)),
-                                          Text(
-                                              "Species ID: ${listOfIntroduced[index]['id']}",
-                                              style: const TextStyle(
-                                                  fontSize: 16)),
-                                          Text(
-                                              "Date Observed ${listOfIntroduced[index]['observed_on_details']['date']}"),
-                                        ],
-                                      ),
-                                    );
-                            },
-                          );
-                        })
-                  ],
-                ),
-              ),
-            )),
+            body:
+
+                // Center(
+                //     child: SingleChildScrollView(
+                Container(
+              padding: EdgeInsets.all(15),
+              // padding: const EdgeInsets.all(10.0),
+              // child: Column(
+              // crossAxisAlignment: CrossAxisAlignment.start,
+              // mainAxisAlignment: MainAxisAlignment.start,
+              // children: <Widget>[
+              // NavButtons(),
+              // const Text(
+              //   'Introduced Plants Observed in July 2022',
+              //   style:
+              //       TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              // ),
+              // Text(
+              //   'Observation Count: ${introducedResponse['total_results'].toString()}',
+              //   style: const TextStyle(
+              //       fontSize: 18, fontWeight: FontWeight.bold),
+              // ),
+              child: FutureBuilder(
+                  future: getIntroduced(),
+                  builder: (context, snapshot) {
+                    return ListView.builder(
+                      itemCount: listOfIntroduced.length,
+                      shrinkWrap: true,
+                      itemBuilder: (context, index) {
+                        return listOfIntroduced[index]['species_guess'] == null
+                            ? Container()
+                            : Container(
+                                padding:
+                                    const EdgeInsets.symmetric(vertical: 10),
+                                // height: 100,
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: <Widget>[
+                                    // Image.network(listOfIntroduced[index]['images']['url']),
+                                    Text(
+                                        "Common Name: ${listOfIntroduced[index]['species_guess']}",
+                                        style: const TextStyle(
+                                            fontSize: 16,
+                                            fontWeight: FontWeight.bold)),
+                                    Text(
+                                        "Species Name: ${listOfIntroduced[index]['taxon']['name']}",
+                                        style: const TextStyle(
+                                            fontSize: 16,
+                                            fontStyle: FontStyle.italic)),
+                                    Text(
+                                        "Species ID: ${listOfIntroduced[index]['id']}",
+                                        style: const TextStyle(fontSize: 16)),
+                                    Text(
+                                        "Date Observed ${listOfIntroduced[index]['observed_on_details']['date']}"),
+                                  ],
+                                ),
+                              );
+                      },
+                    );
+                  })
+              // ]
+              ,
+            ),
+            // ),
+            // )),
           );
   }
 }
--- inaturalist_challenge/lib/views/project_view.dart
+++ inaturalist_challenge/lib/views/project_view.dart
@@ -14,68 +14,58 @@ class ProjectView extends StatefulWidget {
 
 class _ProjectViewState extends State<ProjectView> {
   @override
-
   @override
   Widget build(BuildContext context) {
-    return 
-        Scaffold(
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: Column(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              const Text(
-                'Projects in Bermuda',
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-              ),
-              FutureBuilder(
-                future: getProjects(),
-                builder: (context, snapshot) {
-                  return ListView.builder(
-                    itemCount: listOfProjects.length,
-                    shrinkWrap: true,
-                    itemBuilder: (context, index) {
-                      return Container(
-                        padding: const EdgeInsets.only(top: 10.0, bottom: 10.0),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          mainAxisSize: MainAxisSize.max,
-                          children: <Widget>[
-                            // Image.network(
-                            //     listOfProjects[index]['header_image_url']),
-                            Text(
-                              "Project Title: ${listOfProjects[index]['title']}",
-                              style: const TextStyle(
-                                  fontSize: 16, fontWeight: FontWeight.bold),
-                            ),
-                            Text(
-                              "Project Description: ${listOfProjects[index]['description']}",
-                              style: const TextStyle(fontSize: 16),
-                            ),
+    return Scaffold(
+      body:
+          Container(
+        padding: const EdgeInsets.all(15),
+        // children: <Widget>[
+        //   const Text(
+        //     'Projects in Bermuda',
+        //     style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        //   ),
+        child: FutureBuilder(
+          future: getProjects(),
+          builder: (context, snapshot) {
+            return ListView.builder(
+              itemCount: listOfProjects.length,
+              // shrinkWrap: true,
+              scrollDirection: Axis.vertical,
+              itemBuilder: (context, index) {
+                return Container(
+                  padding: const EdgeInsets.only(top: 10.0, bottom: 10.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    mainAxisSize: MainAxisSize.max,
+                    children: <Widget>[
+                      // Image.network(
+                      //     listOfProjects[index]['header_image_url']),
+                      Text(
+                        "Project Title: ${listOfProjects[index]['title']}",
+                        style: const TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        "Project Description: ${listOfProjects[index]['description']}",
+                        style: const TextStyle(fontSize: 16),
+                      ),
 
-                            Text(
-                              "Observers Count: ${listOfProjects[index]['user_ids'].length}",
-                              style: const TextStyle(fontSize: 16),
-                            ),
-                            // Text(
-                            //     "Observation Count: ${listOfProjects[index]['observed_taxa_count']}")
-                          ],
-                        ),
-                      );
-                    },
-                  );
-                },
-              )
-            ],
-          ),
+                      Text(
+                        "Observers Count: ${listOfProjects[index]['user_ids'].length}",
+                        style: const TextStyle(fontSize: 16),
+                      ),
+                      // Text(
+                      //     "Observation Count: ${listOfProjects[index]['observed_taxa_count']}")
+                    ],
+                  ),
+                );
+              },
+            );
+          },
         ),
       ),
-
-      // ),
     );
   }
 }
